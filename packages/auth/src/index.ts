a0ff94a168ba5717e522e87ad6186e109320b141
yeabnoah <yeabnoah5@gmail.com>
2026-03-09 19:41:30 +0300

    initial commit

---
+++ packages/auth/src/index.ts
@@ -0,0 +1,17 @@
+import prisma from "@Batman/db";
+import { env } from "@Batman/env/server";
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { nextCookies } from "better-auth/next-js";
+
+export const auth = betterAuth({
+  database: prismaAdapter(prisma, {
+    provider: "postgresql",
+  }),
+
+  trustedOrigins: [env.CORS_ORIGIN],
+  emailAndPassword: {
+    enabled: true,
+  },
+  plugins: [nextCookies()],
+});
